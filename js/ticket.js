dc74189a680bcd793ee6bd9fdb90a3227ebdfae4
MD Rokibul Islam Mollah <rokibulislamalways101@gmail.com>
2024-02-19 23:34:46 +0600

    first commit

---
+++ js/ticket.js
@@ -0,0 +1,104 @@
+let totalPrice = 0;
+let grandTotal = 0;
+
+document.querySelectorAll('.kbd').forEach(seat => {
+  seat.addEventListener('click', () => {
+    const selectedSeats = document.querySelectorAll('.selected');
+    if (!seat.classList.contains('selected') && selectedSeats.length < 4) {
+      seat.classList.add('selected');
+      seat.style.backgroundColor = '#1DD100';
+      seat.style.color = 'white';
+      let seatInfo = document.createElement('div');
+      seatInfo.style.display = 'flex';
+      seatInfo.style.justifyContent = 'space-between';
+      seatInfo.innerHTML = `<p>${seat.textContent}</p><p>Economy</p><p>550</p>`;
+      document.querySelector('.selected-seats').appendChild(seatInfo);
+      totalPrice += 550;
+      grandTotal += 550;
+      document.querySelector(
+        '.total-price span'
+      ).textContent = `BDT ${totalPrice}:00`;
+      document.querySelector(
+        '.grand-total span'
+      ).textContent = `BDT ${grandTotal}:00`;
+    }
+  });
+});
+
+const apply = document.getElementById('apply-btn');
+
+apply.addEventListener('click', function (e) {
+  const couponElement = document.getElementById('coupon').value;
+  const totalTicketPrice = document.getElementById('total-price').innerText;
+  const totalTicketPriceNumber = parseInt(totalTicketPrice);
+
+  if (totalPrice === 2200) {
+    if (couponElement === 'NEW15') {
+      const grandTotalElement = document.getElementById('grand-total');
+      const discount = totalTicketPriceNumber * 0.15;
+      const grandTotal = totalTicketPriceNumber - discount;
+      grandTotalElement.innerText = `BDT ${grandTotal}:00`;
+      hideElementById('coupon-area');
+    } else if (couponElement === 'Couple20') {
+      const grandTotalElement = document.getElementById('grand-total');
+      const discount = totalTicketPriceNumber * 0.2;
+      const grandTotal = totalTicketPriceNumber - discount;
+      grandTotalElement.innerText = `BDT ${grandTotal}:00`;
+      hideElementById('coupon-area');
+    } else {
+      alert('Invalid Coupon');
+    }
+  } else {
+    alert('Please select 4 seats');
+  }
+});
+
+// // Coupon Code Section
+// document.querySelector('.btn').addEventListener('click', () => {
+//   const couponInput = document.querySelector('input').value;
+//   if (couponInput === 'NEW15' || couponInput === 'Couple 20') {
+//     const discount = couponInput === 'NEW15' ? totalPrice * 0.15 : totalPrice * 0.2;
+//     grandTotal = totalPrice - discount;
+//     document.querySelector(
+//       '.grand-total span'
+//     ).textContent = `BDT ${grandTotal}:00`;
+//     document.querySelector('.discount').style.display = 'none';
+//   }
+// });
+
+// function hideElementById(elementId) {
+//   const element = document.getElementById(elementId);
+//   element.classList.add('hidden');
+// }
+
+// // Seat Count and Left
+let count = 0;
+let countLess = 40;
+
+const allBtn = document.getElementsByClassName('kbd');
+
+for (const btn of allBtn) {
+  btn.addEventListener('click', function (e) {
+    if (count < 4 && countLess > 36) {
+      count = count + 1;
+      countLess = countLess - 1;
+      document.getElementById('seat-P').innerText = count;
+      document.getElementById('seat-L').innerText = countLess;
+    }
+  });
+}
+
+// Get the modal element open and close
+const modal = document.getElementById('my_modal_1');
+
+const openButton = document.querySelector('[onclick="my_modal_1.showModal()"]');
+
+const continueButton = modal.querySelector('button');
+
+continueButton.addEventListener('click', () => {
+  modal.close();
+});
+
+openButton.addEventListener('click', () => {
+  modal.showModal();
+});
